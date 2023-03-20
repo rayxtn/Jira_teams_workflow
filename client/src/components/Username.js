75eefa4e3be418a6c24f5a9c4cc0225c5c72282d
rayhouimli <houimliraed@outlook.fr>
2023-03-20 03:09:54 +0100

    loginui done

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import logo from '../assets/logotest.png';
+import styles from '../styles/Username.modules.css';
 
 export default function username() {
   return (
     <div className="container mx-auto">
             <div className='flex justify-center items-center h-screen'>
+            <div className={styles.glass}>
                      <div className='title flex flex-col items-center'>
 
                                    <h4 className='text-5xl font-bold'>Hello Again !</h4>
@@ -13,14 +17,18 @@ export default function username() {
 
                 <form className='py-1'>
                 <div className='profile flex justify-center py-4 '>
-                  <img src='' alt='avatar' />
+                  <img src={logo} className={styles.profile_img} alt='avatar' />
                 </div>
                    <div className="textbox">
-                           <input type='text' placeholder='User Name' />
-                           <button type='submit'>Login!</button>
+                           <input className={styles.textbox} type='text' placeholder='User Name' />
+                           <button className={styles.btnw} type='submit'> Login!</button>
+                   </div>
+                   <div className='text-center py-4'>
+                   <span className='text-gray-500'>No account ? contact the admin or  <Link className='text-red-500' to='/Register'>Register now</Link>   </span>
                    </div>
                 </form>
 
+    </div>
     </div>
     </div>
     </div>
